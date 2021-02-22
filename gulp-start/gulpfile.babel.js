1e3382df733e7586f2554846941b2beded8eea03
changxinaglin <281532612@qq.com>
2021-02-22 18:37:58 +0800

    add pulgins

--- gulp-start/gulpfile.babel.js
+++ gulp-start/gulpfile.babel.js
@@ -1,12 +1,13 @@
-const { series } = require('gulp')
+const { src, dest } = require('gulp')
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify')
+const rename = require('gulp-rename');
 
-function clean(cb) {
-  cb()
+exports.default = function () {
+  return src('static/**/*.js')
+    .pipe(babel())
+    .pipe(uglify())
+    .pipe(dest('build/'))
+    .pipe(rename({ extname: '.min.js' }))
+    .pipe(dest('build/'))
 }
-
-function build(cb) {
-  cb()
-}
-
-exports.build = build
-exports.default = series(clean, build)
